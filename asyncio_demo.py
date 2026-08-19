868e040ff44fddc94613032c4c1f045ebd43cfeb
Suruthi24 <suruthiravi2405@gmail.com>
2026-08-19 08:38:34 +0530

    basics python scripts and libraries

---
+++ asyncio_demo.py
@@ -0,0 +1,15 @@
+import __main__
+import asyncio
+
+async def task(name):
+    print(name, "started")
+    await asyncio.sleep(2)
+    print(name, "finished")
+
+async def main():
+    await asyncio.gather(
+        task("Task 1"),
+        task("Task 2"),
+        task("Task 3")
+    )
+asyncio.run(main())
